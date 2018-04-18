ee055b2e3c8c3d9c63c47a92f74896e58ad520ae
Dan Reuman <reuman@ku.edu>
2018-04-17 21:15:02 -0500

    finished test file for mmsclean, at least for now

--- tests/testthat/tests_mmsclean.R
+++ tests/testthat/tests_mmsclean.R
@@ -41,3 +41,17 @@ test_that("test error catching functionality of mmsclean", {
   expect_error(mmsclean(mats,resp,pred,n),
                "Error in mmsclean: n out of range",fixed=T)
 })
+
+test_that("test the output of mmsclean", {
+  #test 1
+  mats<-list(v1=matrix(1:9,3,3),v2=matrix(10:18,3,3),v3=matrix(19:27,3,3))
+  resp<-2
+  pred<-c(1,3)
+  n<-1
+  h<-mmsclean(mats,resp,pred,n)
+  exp.ans<-list(mats=list(v2=matrix(c(NA,11,12,NA,NA,15,NA,NA,NA),3,3),
+                          v1=matrix(c(NA,2,3,NA,NA,6,NA,NA,NA),3,3),
+                          v3=matrix(c(NA,20,21,NA,NA,24,NA,NA,NA),3,3)),
+                resp=1,pred=c(2,3))
+  expect_equal(h,exp.ans)
+})
