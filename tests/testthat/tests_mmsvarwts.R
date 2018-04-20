3d07a9bcdb0fb473e89ed38d7b36688cc716a56b
jonathan-walter <jaw3es@virginia.edu>
2018-04-20 14:17:32 -0400

    reconciling model.name vs. model.names, updates to mmsvarwts tests

--- tests/testthat/tests_mmsvarwts.R
+++ tests/testthat/tests_mmsvarwts.R
@@ -1,14 +1,29 @@
 context("mmsvarwts")
 
-#test_that("tests for mmsvarwts", {
-#  #test 1
-#  mats<-list(resp=matrix(1:9,3,3),pred1=matrix(10:18,3,3),pred2=matrix(19:27,3,3))
-#  resp<-1
-#  pred<-c(2,3)
-#  n<-1
-#  varnames<-c("response","predictor1")
-#  h<-mmsmodwts(mats, nrand=5, n, maxruns=5)
-#  expect_error(mmsvarwts(pred,h , varnames),
-#               "Error in mmsvarwts: varnames must be the same length as pred", fixed=T)
-#  
-#})
+test_that("test error catching functionality of mssvarwts", {
+ #test 1
+ pred<-c(2,3)
+ varnames<-c("response","predictor1")
+ h<-list(model.names=c("2","3","2:3"),
+         freq.top=rpois(3,3)
+ )
+ expect_error(mmsvarwts(pred, h, varnames),
+              "Error in mmsvarwts: varnames must be one element longer than pred", fixed=T)
+ #test 2
+ pred<-c(2,3)
+ varnames<-c("response","predictor1","predictor2")
+ h<-list(model.names=c("2","3","2:3"))
+ expect_error(mmsvarwts(pred, h, varnames),
+              "Error in msvarwts: weights must contain model.names and freq.top", fixed=T)
+ 
+})
+
+test_that("test mmwvarwts in an arbitrary test case that should ome out the same on future runs", {
+  set.seed(5117)
+  pred<-c(2,3)
+  varnames<-c("response", "predictor1", "predictor2")
+  wts<-list(model.names=c("2","3","2:3"),
+          freq.top=rpois(3,3))
+  h<-mmsvarwts(pred, wts, varnames)
+  expect_known_hash(h, hash="ebcf5e84286c5f297e07b61221b52a0c")        
+})
