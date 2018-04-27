b6b084f26e0982f34e2f216359da12e714d2fed3
Dan Reuman <reuman@ku.edu>
2018-04-27 15:28:46 -0500

    revised matregtest and its tests

--- R/matregtest.R
+++ R/matregtest.R
@@ -2,9 +2,8 @@
 #' 
 #' A function for doing matrix regression tests of a model against a nested model.
 #' 
-#' @param mats A named list of matrices, all assumed to be the same dimensions and symmetric. Diagonals are not used. Off-diagonal NA/NaNs/Infs/etc not allowed. 
-#' @param resp The index in mats of the response variable (input is a numeric value, e.g. resp = 1)
-#' @param pred The indices in mats of predictor variables in the more complex of the two models to be compared, should not include resp. Input is numeric value(s), e.g. pred=1, pred =1:2, pred =c(1,2,4).
+#' @param mats A list of numeric matrices, all assumed to be the same dimensions and symmetric. Diagonals are not used. Off-diagonal non-finite values not allowed. The first entry taken to be the response.
+#' @param pred The indices in mats of predictor variables in the more complex of the two models to be compared, should not include 1. Input is numeric value(s), e.g. pred=2, pred =2:3, pred =c(2,3,5).
 #' @param drop The indices in mats of predictor variables that are dropped to get from the complex to the simple model. Should be a subset of pred. 
 #' @param numperm The number of permutations used to do the test
 #' 
@@ -30,70 +29,26 @@
 #' pred<-2:4
 #' drop<-4
 #' numperm<-100
-#' h<-matregtest(mats,resp,pred,drop,numperm)
+#' h<-matregtest(mats,pred,drop,numperm)
 #
 #' @export
 
 
-matregtest<-function(mats,resp,pred,drop,numperm)
+matregtest<-function(mats,pred,drop,numperm)
 {
-  #some simple error checking
-  if (!(resp %in% 1:length(mats)))
-  {
-    stop("Error in matregtest: response variable index out of bounds")
-  }
-  if (length(pred)==0)
-  {
-    stop("Error in matregtest: at least one value required in pred")
-  }
-  if (!all(pred %in% 1:length(mats)))
-  {
-    stop("Error in matregtest: predictor variable index out of bounds")
-  }
-  if (resp %in% pred)
-  {
-    stop("Error in matregtest: resp cannot also be in pred")
-  }
+  #some error checking
+  errcheck_mats("matregtest",mats)
+  errcheck_pred("matregtest",list(pred),length(mats))
   if (!all(drop %in% pred))
   {
     stop("Error in matregtest: drop should be a subset of pred")
   }
-  mats<-mats[c(resp,pred)]
-  if(is.null(names(mats))){names(mats)<-c("y",paste0("x",1:length(pred)))}
-  resp<-1
+
+  #throw out what you don't need
+  mats<-mats[c(1,pred)]
   drop<-which(pred %in% drop)+1
   pred<-2:length(mats)
 
-  #further error checking - check all the matrices are the same dimension, and square
-  d1<-sapply(X=mats,FUN=function(x){return(dim(x)[1])}) 
-  d2<-sapply(X=mats,FUN=function(x){return(dim(x)[2])}) 
-  if (!all(d1[2:length(d1)]==d1[1]) || !all(d2[2:length(d2)]==d2[1]) ||
-      d1[1]!=d2[1])
-  {
-    stop("Error in matregtest: all matrices must be same dimension and square")
-  }
-  d<-unname(d1[1])
-  
-  #further error checking - make sure all the matrices are symmetric
-  for (counter in 1:length(mats))
-  {
-    if (!isSymmetric(mats[[counter]]))
-    {
-      stop("Error in matregtest: all matrices must be symmetric")
-    }
-  }
-  
-  #further error checking - screen for off-diagonal NAs
-  for (counter in 1:length(mats))
-  {
-    h<-mats[[counter]]
-    diag(h)<-0
-    if (!all(is.finite(h)))
-    {
-      stop("Error in matregtest: non-finite off diagonal entries not allowed")
-    }
-  }
-  
   #throw out the diagonals
   for (counter in 1:length(mats))
   {
@@ -101,6 +56,7 @@ matregtest<-function(mats,resp,pred,drop,numperm)
   }
   
   #get the regression formula for the complex model
+  if(is.null(names(mats))){names(mats)<-c("y",paste0("x",1:length(pred)))}
   form.com<-paste0(names(mats)[1],"~")
   if (length(mats)<3)
   {
--- tests/testthat/tests_matregtest.R
+++ tests/testthat/tests_matregtest.R
@@ -1,65 +1,5 @@
 context("matregtest")
 
-test_that("test error catching functionality of matregtest", {
-  mats<-list(v1=matrix(1:16,4,4),v2=matrix(17:32,4,4),v3=matrix(33:48,4,4))
-  resp<-4
-  pred<-c(2,3)
-  drop<-2
-  numperm<-100
-  expect_error(matregtest(mats,resp,pred,drop,numperm),
-               "Error in matregtest: response variable index out of bounds",fixed=T)
-
-  resp<-1
-  pred<-c()
-  drop<-2
-  numperm<-100
-  expect_error(matregtest(mats,resp,pred,drop,numperm)
-               ,"Error in matregtest: at least one value required in pred")
-  
-  resp<-1
-  pred<-2:4
-  drop<-2
-  numperm<-100
-  expect_error(matregtest(mats,resp,pred,drop,numperm),
-               "Error in matregtest: predictor variable index out of bounds",fixed=T)
-  
-  resp<-1
-  pred<-1:3
-  drop<-2
-  numperm<-100
-  expect_error(matregtest(mats,resp,pred,drop,numperm),
-               "Error in matregtest: resp cannot also be in pred",fixed=T)
-  
-  resp<-1
-  pred<-2
-  drop<-3
-  numperm<-100
-  expect_error(matregtest(mats,resp,pred,drop,numperm),
-               "Error in matregtest: drop should be a subset of pred")
-  
-  mats<-list(v1=matrix(1:16,4,4),v2=matrix(17:32,4,4),v3=matrix(5,5,5))
-  resp<-1
-  pred<-2:3
-  drop<-3
-  numperm<-100
-  expect_error(matregtest(mats,resp,pred,drop,numperm),
-               "Error in matregtest: all matrices must be same dimension and square",fixed=T)
-  
-  mats<-list(v1=matrix(1:16,4,4),v2=matrix(17:32,4,4),v3=matrix(5,4,4))
-  resp<-1
-  pred<-2:3
-  drop<-3
-  numperm<-100
-  expect_error(matregtest(mats,resp,pred,drop,numperm),
-               "Error in matregtest: all matrices must be symmetric")
-  
-  mats<-list(v1=matrix(1,4,4),v2=matrix(2,4,4),v3=matrix(5,4,4))
-  mats[[1]][1,2]<-NA
-  mats[[1]][2,1]<-NA
-  expect_error(matregtest(mats,resp,pred,drop,numperm),
-               "Error in matregtest: non-finite off diagonal entries not allowed")
-})
-
 test_that("test matregtest in a perfect-regression case", {
   set.seed(101)
   v2<-matrix(rnorm(100),10,10)
@@ -70,13 +10,18 @@ test_that("test matregtest in a perfect-regression case", {
   v4<-v4+t(v4)
   v1<-1*v2+2*v3+3*v4+1
   mats<-list(v1=v1,v2=v2,v3=v3,v4=v4)
-  resp<-1
   pred<-2:4
   drop<-4
   numperm<-10
-  h<-matregtest(mats,resp,pred,drop,numperm)
+  h<-matregtest(mats,pred,drop,numperm)
   expect_equal(h$ssr_dat,0)
   expect_equal(h$p,0)
+})
+
+test_that("test matregtest error catching", {
+  pred<-2:3
+  drop<-4
+  expect_error(matregtest(mats,pred,drop,numperm),"Error in matregtest: drop should be a subset of pred")
 })
 
 test_that("test matregtest in arbitrary test cases that should come out the same on future runs", {
@@ -93,16 +38,15 @@ test_that("test matregtest in arbitrary test cases that should come out the same
   err<-err+t(err)
   v1<-1*v2+2*v3+1+err
   mats<-list(v1=v1,v2=v2,v3=v3,v4=v4,v5=v5)
-  resp<-1
   pred<-2:5
   drop<-4:5
   numperm<-100
-  h<-matregtest(mats,resp,pred,drop,numperm)
+  h<-matregtest(mats,pred,drop,numperm)
   #I got this hash with digest::digest(h)
   expect_known_hash(h,hash="4a1352751f6e936ff58e1d17fa92c5c6")
   
   drop<-2:3
-  h<-matregtest(mats,resp,pred,drop,numperm)
+  h<-matregtest(mats,pred,drop,numperm)
   #I got this hash with digest::digest(h)
   expect_known_hash(h,hash="a17b5db17284e14f7c8c8e8224a7c9a3")
 })
